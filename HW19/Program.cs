dfd25a7cfec80046637ab600019d10ee4ff6dc78
Barker38 <auchanin@inbox.ru>
2022-08-18 15:29:13 +0800

    HW21

---
+++ HW19/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+int number = 0;
+while (true)
+{
+    Console.WriteLine("Введите пятизначное число ");
+    if (int.TryParse(Console.ReadLine(), out number)) break;
+    else
+    {
+        Console.WriteLine("Введите корректное число ");
+    }
+}
+
+bool GetPalindrom(int num)
+{
+    int oldValue = num;
+    int newValue = 0;
+    while (num > 0 || num < 0)
+    {
+        int ostatok = num % 10;
+        newValue = newValue * 10 + ostatok;
+        num = num / 10;
+    }
+    return Math.Abs(newValue) == Math.Abs(oldValue);
+}
+
+if (GetPalindrom(number)) Console.WriteLine($"{number} -> Да");
+else Console.WriteLine($"{number} -> Нет");
---
+++ HW23/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число N для вывода таблицы кубов ");
+int N = int.Parse(Console.ReadLine());
+int a = 0;
+
+void Cube(int b)
+{
+    for (int i = 1; i <= b; ++i)
+        Console.WriteLine($"{i} | {i * i * i}");
+}
+
+if (N < 1)
+{
+    Console.WriteLine("Введите число >= 1");
+    return;
+}
+
+Cube(N);
+
