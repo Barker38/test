00ef7531c61b36457a5830f8931f05b6ae730b74
Barker38 <auchanin@inbox.ru>
2022-08-26 11:44:43 +0800

    Добавил домашние задания 5-го Урока

---
+++ HW34/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size) //Генерируем массив рандомных чисел
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(100, 999);
+    }
+    return array;
+}
+
+
+
+int CountNumbers(int[] array) //Находим нечетные индекса
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+
+    }
+    return count;
+}
+
+void PrintArray(int[] array) //Вывод массива
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write(array[i]);
+    }
+
+    Console.Write("]");
+}
+
+int[] arr = CreateArrayRndInt(5);
+int resultnumbers = CountNumbers(arr);
+
+PrintArray(arr);
+Console.Write($" -> {resultnumbers}");
+
