adfdc8490c025358c0ca4db9ed840990688d7d2f
Barker38 <auchanin@inbox.ru>
2022-09-05 11:39:00 +0800

    HW52

---
+++ HW52/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 52. 
+// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateMatrixRndInt(int row, int col, int min, int max) // Метод создания двумерного массива
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random(); // Создаем объект рандом
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // Забираем количество строк в матрице
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // Забираем количество столбцов матрицы
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4},");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void FindSrArich(int[,] matrix)
+{
+    double avgnum = 0;
+    Console.Write($"Среднее арифметическое: ");
+
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            avgnum += matrix[j, i];
+        }
+        avgnum /= matrix.GetLength(1);
+        Console.Write(i == matrix.GetLength(1) - 1 ? $"{Math.Round(avgnum, 2)}. " : $"{Math.Round(avgnum, 2)}; ");
+        avgnum = 0;
+    }
+}
+
+int[,] newMatrix = CreateMatrixRndInt(4, 4, 1, 9);
+PrintMatrix(newMatrix);
+FindSrArich(newMatrix);
